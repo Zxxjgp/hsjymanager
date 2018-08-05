77fd66bb049eee7e3f0419dfb6f630a11207e3e9
jgp <948102903@qq.com>
2018-08-05 11:29:48 +0800

    1

--- src/main/java/com/hsjy/manager/hsjymanager/controller/IndexController.java
+++ src/main/java/com/hsjy/manager/hsjymanager/controller/IndexController.java
@@ -1,29 +1,48 @@
 package com.hsjy.manager.hsjymanager.controller;
 
-import com.hsjy.manager.hsjymanager.entity.Test;
-import com.hsjy.manager.hsjymanager.service.TestService;
+import com.hsjy.manager.hsjymanager.utils.result.Result;
+import com.hsjy.manager.hsjymanager.utils.result.ResultGenerator;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.annotation.Resource;
-import java.util.List;
-
+/**
+ * 首页 业务处理
+ * 
+ * @author ruoyi
+ */
 @Controller
-public class IndexController {
-    @Resource
-    private TestService testService;
-    @GetMapping("/list")
-    public String index(){
+public class IndexController
+{
 
-        return "indexs";
+    @GetMapping("/index")
+    public String index()
+    {
+        return "login";
     }
-    @RequestMapping("login")
+
+    @GetMapping("/helloworld")
     @ResponseBody
-    public List<Test> all(){
-        return testService.findall();
+    public Result helloWorld() {
+        return ResultGenerator.genSuccessResult("helloworld");
     }
+    @GetMapping("/doLogin")
+    @ResponseBody
+    public Result doLogin(String username, String password) {
 
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+        try {
+            subject.login(token);
+        } catch (AuthenticationException e) {
+            token.clear();
+            return ResultGenerator.genFailResult("登录失败，用户名或密码错误！");
+        }
+        return ResultGenerator.genSuccessResult("登录成功");
+    }
 }
